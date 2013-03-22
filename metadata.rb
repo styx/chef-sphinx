76a8cc3a4b65816fa9831af9d301890294f28fa3
Mikhail S. Pobolovets <styx.mp@gmail.com>
2013-03-22 16:04:48 +0300

    Added metadata files.
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Mikhail Pobolovets"
+maintainer_email "styx.mp@gmail.com"
+license          "Apache 2.0"
+description      "Installs Sphinx search engine."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1"
+
+depends "build-essential"
+depends "postgresql"
+depends "mysql"
